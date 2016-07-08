d301d5c183933797daa877b9521f4bec6c29718f
Courtney Moore <cmoore@DHF-MBA-32.local>
2016-07-08 16:05:47 -0400

    saving

---
+++ page4-app.js
@@ -0,0 +1,104 @@
+var r = confirm("You are right");
+if (r == true) {
+    x = "You are wrong";
+}
+
+var bob = document.getElementById("derp");
+
+bob.addEventListener("click",function(){alert('Your right!')},false);
+
+var bob = document.getElementById("dude");
+
+bob.addEventListener("click",function(){alert('your wrong')},true);
+
+
+var bob = document.getElementById("Chirp");
+
+bob.addEventListener("click",function(){alert('Your right!')},false)
+
+var bob = document.getElementById("cat");
+
+bob.addEventListener("click",function(){alert('your wrong')},true);
+
+var bob = document.getElementById("sluggy");
+
+bob.addEventListener("click",function(){alert('Your right!')},false);
+
+var bob = document.getElementById("pc");
+
+bob.addEventListener("click",function(){alert('Your wrong')},false);
+
+var bob = document.getElementById("dog");
+
+bob.addEventListener("click",function(){alert('Your right')},true);
+
+var bob = document.getElementById("zebra");
+
+bob.addEventListener("click",function(){alert('Your wrong')},false);
+
+var bob = document.getElementById("quiy");
+
+bob.addEventListener("click"),function(){alert}('your right')},true);
+
+var bob = document.getElementById("John");
+
+bob.addEventListener("click",function(){alert('Your wrong')},false);
+
+var bob = document.getElementById("pika");
+
+bob.addEventListener("click"),function(){alert}('your right')},true);
+
+var bob = document.getElementById("jerp");
+
+bob.addEventListener("click",function(){alert('Your wrong')},false);
+
+var bob = document.getElementById("dodaweewoo");
+
+bob.addEventListener("click"),function(){alert}('your right')},true);
+
+var bob = document.getElementById("FIVE");
+
+bob.addEventListener("click",function(){alert('Your wrong')},false);
+;
+var bob = document.getElementById("car");
+
+bob.addEventListener("click"),function(){alert}('your right')},true);
+
+var bob = document.getElementById("curp");
+
+bob.addEventListener("click",function(){alert('Your wrong')},false);
+
+var bob = document.getElementById("photo");
+
+bob.addEventListener("click"),function(){alert}('your right')},true);
+
+var bob = document.getElementById("photo2");
+
+bob.addEventListener("click",function(){alert('Your wrong')},false);
+
+var bob = document.getElementById("beat");
+
+bob.addEventListener("click"),function(){alert}('your right')},true);
+
+var bob = document.getElementById("beaty");
+
+bob.addEventListener("click",function(){alert('Your wrong')},false);
+
+var bob = document.getElementById("11");
+
+bob.addEventListener("click"),function(){alert}('your right')},true);
+
+var bob = document.getElementById("");
+
+bob.addEventListener("click",function(){alert('Your wrong')},false);
+
+
+
+
+
+
+
+
+
+
+
